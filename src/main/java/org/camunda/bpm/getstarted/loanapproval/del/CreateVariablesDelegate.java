a9b81d5e2b4806d547b742048aaa045dc8658af8
Dominik Kriese <dominik.kriese@novatec-gmbh.de>
2022-03-28 11:59:47 +0200

    Build process to crash

---
+++ src/main/java/org/camunda/bpm/getstarted/loanapproval/del/CreateVariablesDelegate.java
@@ -0,0 +1,27 @@
+package org.camunda.bpm.getstarted.loanapproval.del;
+
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class CreateVariablesDelegate implements JavaDelegate {
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+        var numberOfVariables = 50;
+        var numberOfProcesses = 50;
+
+        List<String> instances = new ArrayList<>();
+        for (int i = 0; i < numberOfProcesses; i++) {
+            instances.add("" + i);
+        }
+        delegateExecution.setVariable("instances", instances);
+        for (int i = 0; i < numberOfVariables; i++) {
+            delegateExecution.setVariable("var" + i, "val" + i);
+        }
+    }
+}
